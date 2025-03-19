7871fc349dd1379b043dbb2663c82d5a83b0d984
yeon <skykys8@gmail.com>
2025-03-19 16:37:36 +0900

    feat : 모임개설관련 모델 및 리포지터리 추가

---
+++ src/main/java/org/codenova/studymate/model/StudyMember.java
@@ -0,0 +1,17 @@
+package org.codenova.studymate.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+public class StudyMember {
+    private int id;
+    private String userId;
+    private String groupId;
+    private String role;
+    private LocalDateTime appliedAt;
+    private LocalDateTime joinedAt;
+}
